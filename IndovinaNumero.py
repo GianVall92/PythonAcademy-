04048073b25b106f8a9711386cacf1558fc39e47
GianVall92 <gian.valletta@studenti.unina.it>
2021-03-04 10:00:12 +0100

    Inserimento commenti

--- IndovinaNumero.py
+++ IndovinaNumero.py
@@ -4,9 +4,9 @@ flag = True
 
 while flag:
     risposta = int (input("Indovina il numero: "))
-    if risposta == NumeroRandom:
+    if risposta == NumeroRandom: #Condizione di numero indovinato
         print ("Bravo")
-    else:
+    else: #Nel caso in cui sia sbagliato o è più piccolo o più grande
        if (risposta > NumeroRandom):
            print ("Il numero è più piccolo")
        else:
